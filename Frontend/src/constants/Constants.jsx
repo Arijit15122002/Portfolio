f3cfa83e9946dab759697bfbeeb56d7ef6221dec
Arijit15122002 <biswasarijit2002@gmail.com>
2024-06-29 02:26:40 +0530

    Finished

--- Frontend/src/constants/Constants.jsx
+++ Frontend/src/constants/Constants.jsx
@@ -21,7 +21,7 @@ export const SLIDES = [
                 <div className="w-auto h-auto p-4 ml-2 mt-2 bg-violet-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-notebook-text"><path d="M2 6h4"/><path d="M2 10h4"/><path d="M2 14h4"/><path d="M2 18h4"/><rect width="16" height="20" x="4" y="2" rx="2"/><path d="M9.5 8h5"/><path d="M9.5 12H16"/><path d="M9.5 16H14"/></svg>
                 </div>
-                <a href="" className="w-auto h-auto p-4 mt-2 mr-2 bg-violet-400 inline-block rounded-full">
+                <a href="https://github.com/Arijit15122002/Alfredough" className="w-auto h-auto p-4 mt-2 mr-2 bg-violet-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
                 </a>
             </div>
@@ -34,9 +34,6 @@ export const SLIDES = [
                     <div className="text-violet-800 dark:text-violet-300 px-8 text-xl kanit">
                         Unveiling Alfredough, your one-stop platform for creating and sharing your voice! Blog, discover, and connect with fellow writers - all in one vibrant online space. Join Alfredough and let the words flow.
                     </div>
-                </div>
-                <div>
-                    <div></div>
                 </div>
                 <div className="flex flex-row items-center py-4">
                     <div className="text-xl protest text-violet-800 dark:text-violet-300 pl-6 pr-2 py-4">
@@ -58,7 +55,7 @@ export const SLIDES = [
                 <div className="w-auto h-auto p-4 ml-2 mt-2 bg-red-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play"><polygon points="6 3 20 12 6 21 6 3"/></svg>
                 </div>
-                <a href="" className="w-auto h-auto p-4 mt-2 mr-2 bg-red-400 inline-block rounded-full">
+                <a href="https://github.com/Arijit15122002/ViewTube" className="w-auto h-auto p-4 mt-2 mr-2 bg-red-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
                 </a>
             </div>
@@ -71,9 +68,6 @@ export const SLIDES = [
                     <div className="text-red-800 dark:text-red-300 px-8 text-xl kanit">
                     Stream your way to satisfaction with ViewTube! This distraction-free platform lets you search and watch videos powered by YouTube's V3 API. Dive in and enjoy endless entertainment, all on ViewTube.
                     </div>
-                </div>
-                <div>
-                    <div></div>
                 </div>
                 <div className="flex flex-row items-center py-4">
                     <div className="text-xl protest text-red-800 dark:text-red-300 pl-6 pr-2 py-4">
@@ -95,7 +89,7 @@ export const SLIDES = [
                 <div className="w-auto h-auto p-4 ml-2 mt-2 bg-green-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z"/></svg>
                 </div>
-                <a href="" className="w-auto h-auto p-4 mt-2 mr-2 bg-green-400 inline-block rounded-full">
+                <a href="https://github.com/Arijit15122002/Weather-App" className="w-auto h-auto p-4 mt-2 mr-2 bg-green-400 inline-block rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
                 </a>
             </div>
@@ -108,9 +102,6 @@ export const SLIDES = [
                     <div className="text-green-800 dark:text-green-300 px-8 text-xl kanit">
                     Go global with Weatherly! This user-friendly platform lets you check weather conditions for any country seamlessly. Plan your adventures with confidence, all on Weatherly.
                     </div>
-                </div>
-                <div>
-                    <div></div>
                 </div>
                 <div className="flex flex-row items-center py-4">
                     <div className="text-xl protest text-green-800 dark:text-green-300 pl-6 pr-2 py-4">
@@ -145,9 +136,6 @@ export const SLIDES = [
                     <div className="text-blue-800 dark:text-blue-300 px-8 text-xl kanit">
                     A secure and efficient online voting platform built with React and blockchain technology, empowers voters to cast their ballots remotely, ensuring transparency, anonymity, and accessibility to their choices
                     </div>
-                </div>
-                <div>
-                    <div></div>
                 </div>
                 <div className="flex flex-row items-center py-4">
                     <div className="text-xl protest text-blue-800 dark:text-blue-300 pl-6 pr-2 py-4">
